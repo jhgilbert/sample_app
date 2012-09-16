1980a1aa7022f977ad782f66024ec599ace2ce4b
jhgilbert <j.h.gilbert@gmail.com>
2012-09-16 15:09:36 -0700

    Add friendly forwarding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,11 +37,14 @@ class UsersController < ApplicationController
   private
   
     def signed_in_user
-      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+      unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
     end 
     
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
     end
+  end
 end
