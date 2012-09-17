c4ace882c15e7f7cd3b3b8a0cdbf4cd6e6ef0759
jhgilbert <j.h.gilbert@gmail.com>
2012-09-16 18:25:06 -0700

    Fix nesting issue in User controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,13 +38,13 @@ class UsersController < ApplicationController
   
     def signed_in_user
       unless signed_in?
-      store_location
-      redirect_to signin_url, notice: "Please sign in."
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
     end 
     
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
     end
-  end
 end
