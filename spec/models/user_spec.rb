d30c80059d59bcbdf1089812d4dc534b574f963d
jhgilbert <j.h.gilbert@gmail.com>
2012-09-17 00:18:30 -0700

    Add test for user's micropost attribute

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -30,6 +30,9 @@ describe User do
   it { should be_valid }
   it { should_not be_admin }
   
+  it { should respond_to(:authenticate) }
+  it { should respond_to(:microposts) }
+  
   describe "with admin attribute set to 'true'" do
     before do
       @user.save!
