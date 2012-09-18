36a4e0495165d60f8c583d13fc5059afb83e03ce
jhgilbert <j.h.gilbert@gmail.com>
2012-09-18 00:46:56 -0700

    Add a test for unfollowing a user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -38,6 +38,7 @@ describe User do
   it { should respond_to(:followed_users) }
   it { should respond_to(:following?) }
   it { should respond_to(:follow!) }
+  it { should respond_to(:unfollow!) }
   
   describe "with admin attribute set to 'true'" do
     before do
@@ -177,5 +178,12 @@ describe User do
      
     it { should be_following(other_user) }
     its(:followed_users) { should include(other_user) }
+    
+    describe "and unfollowing" do
+      before { @user.unfollow!(other_user) }
+      
+      it { should_not be_following(other_user) }
+      its(:followed_users) { should_not include(other_user) }
+    end
   end
 end
