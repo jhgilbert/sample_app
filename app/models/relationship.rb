155af04394fac1206255a8d36bf356461636818d
jhgilbert <j.h.gilbert@gmail.com>
2012-09-18 00:00:38 -0700

    Add belongs_to association to the Relationship model

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,3 +1,6 @@
 class Relationship < ActiveRecord::Base
-  attr_accessible :followed_id, :follower_id
+  attr_accessible :followed_id # ", :follower_id" disappears in 11.6
+  
+  belongs_to :follower, class_name: "User"
+  belongs_to :followed, class_name: "User"
 end
