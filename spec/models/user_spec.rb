6effe9e1f2c8f470e204cfd4b1382917ad37d0d5
jhgilbert <j.h.gilbert@gmail.com>
2012-09-17 23:52:35 -0700

    Add test for user.relationships attribute and feed attribute

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -33,6 +33,9 @@ describe User do
   it { should respond_to(:authenticate) }
   it { should respond_to(:microposts) }
   
+  it { should respond_to(:feed) }
+  it { should respond_to(:relationships) }
+  
   describe "with admin attribute set to 'true'" do
     before do
       @user.save!
