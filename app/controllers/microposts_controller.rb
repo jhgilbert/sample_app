5426275a00cfd72e42dec7f5f5c8b8394d2c3c82
jhgilbert <j.h.gilbert@gmail.com>
2012-09-17 22:54:40 -0700

    Add empty @feed_items instance variable to the microposts controller

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -10,6 +10,7 @@ class MicropostsController < ApplicationController
       flash[:success] = "Micropost created!"
       redirect_to root_url
     else
+      @feed_items = [] # Not sure if there should be a space between brackets.
       render 'static_pages/home'
 	  end
   end
