0ce7f0be645c784d265402318aaf3fd82e87a02f
jhgilbert <j.h.gilbert@gmail.com>
2012-09-17 23:01:10 -0700

    Fix typo in feed-rendering test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -22,7 +22,7 @@ describe "Static pages" do
     
       it "should render the user's feed" do
         user.feed.each do |item|
-          page.should have_selector("li#{item.id}", text: item.content)
+          page.should have_selector("li##{item.id}", text: item.content)
         end
       end
     end
