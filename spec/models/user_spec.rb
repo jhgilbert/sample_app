900506a6bfdea7c95adf7e54d6c8e32a00a54150
jhgilbert <j.h.gilbert@gmail.com>
2012-09-18 00:19:56 -0700

    Add tests for following utility methods

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -36,6 +36,8 @@ describe User do
   it { should respond_to(:feed) }
   it { should respond_to(:relationships) }
   it { should respond_to(:followed_users) }
+  it { should respond_to(:following?) }
+  it { should respond_to(:follow!) }
   
   describe "with admin attribute set to 'true'" do
     before do
@@ -164,5 +166,15 @@ describe User do
        its(:feed) { should include(older_micropost) }
        its(:feed) { should_not include(unfollowed_post) }
      end
-   end
+     
+     describe "following" do
+       let(:other_user) { FactoryGirl.create(:user) }
+       before do
+         @user.save
+         @user.follow!(other_user)
+       end
+       
+       it { should be_following(other_user) }
+       its(:followed_users) { should include(other_user) }
+     end
  end
